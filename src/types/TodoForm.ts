69baa21b7a7dd480eb7eea6a48ccdc7627bb8325
Hannan Fikri <hannanfikri510@gmail.com>
2025-02-17 21:51:52 +0800

    add types

---
+++ src/types/TodoForm.ts
@@ -0,0 +1,7 @@
+export type TodoForm = {
+  activity: string;
+  price: string;
+  type: string;
+  isRequired: boolean;
+  accessibility: string;
+};
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { TodoForm } from "./TodoForm";
