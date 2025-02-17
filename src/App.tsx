a0fce45b59cc2a89c069e48af64134ee87ad12a1
Hannan Fikri <hannanfikri510@gmail.com>
2025-02-17 21:52:57 +0800

    create usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
 export const App = () => {
+  const type = useMemo(
+    () => [
+      { id: 1, name: "Education" },
+      { id: 2, name: "Recreational" },
+      { id: 3, name: "Social" },
+      { id: 4, name: "Diy" },
+      { id: 5, name: "Charity" },
+      { id: 6, name: "Cooking" },
+      { id: 7, name: "Relaxation" },
+      { id: 8, name: "Music" },
+      { id: 9, name: "Busywork" },
+    ],
+    []
+  );
   return (
     <div>
       <div className="container mx-auto bg-red-50">test</div>
